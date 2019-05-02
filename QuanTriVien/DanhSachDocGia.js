87d34f89e603654b8588eb84899e108f2f04dfb6
hauphan15 <mrhauphan@gmail.com>
2019-05-02 12:31:56 +0700

    thêm hiệu ứng

---
+++ QuanTriVien/DanhSachDocGia.js
@@ -0,0 +1,11 @@
+
+var tb = document.getElementById("tbl");
+for (let i = 1; i < tb.rows.length; i++) {
+    tb.rows[i].onclick = function () {
+        document.getElementById("stt").value = this.cells[0].innerHTML;
+        document.getElementById("email").value = this.cells[1].innerHTML;
+        document.getElementById("ht").value = this.cells[2].innerHTML;
+        document.getElementById("ns").value = this.cells[3].innerHTML;
+        document.getElementById("ncl").value = this.cells[4].innerHTML;
+    };
+}
