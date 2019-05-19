1e811b38459c78da1685a8b47a33734b9f110923
hauphan15 <mrhauphan@gmail.com>
2019-05-19 19:12:48 +0700

    commit

--- routes/chuyenmuc.route.js
+++ routes/chuyenmuc.route.js
@@ -1,12 +1,21 @@
 var exress = require('express');
 var baivietModel = require('../models/baiviet.model');
-var chuyenmuc =require('../models/chuyenmuc.model');
+var chuyenmucModel = require('../models/chuyenmuc.model');
 var router = exress.Router();
 
 
 router.get('/:id/baiviet', (req, res) => {
     var id = req.params.id;
     var p = baivietModel.allByCat(id);
+
+    // //
+    // var q = chuyenmucModel.singel(id);
+    // var row;
+    // q.then(rows => {
+    //     row = rows;
+    //     console.log(row);
+    // });
+    // //
 
     p.then(rows => {
         for (const cm of res.locals.lcChuyenmuc) {
@@ -15,8 +24,8 @@ router.get('/:id/baiviet', (req, res) => {
             }
         }
         res.render('vwBaiviet/byCat', {
-            baiviet: rows2,
-            CM:row[0]
+            baiviet: rows,
+            CM: rows[0]
         });
     }).catch(err => {
         console.log(err);
