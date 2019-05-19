274e1f941853e7774b7d48a4a79ec6cb889ab952
zSimpleMan <43574093+zSimpleMan@users.noreply.github.com>
2019-05-19 21:19:23 +0700

    Update chuyenmuc.model.js

--- models/chuyenmuc.model.js
+++ models/chuyenmuc.model.js
@@ -6,9 +6,7 @@ module.exports = {
     },
 
     allWithDetails: () =>{
-        return db.load(`SELECT cm.ID, cm.ChuyenMuc, cm.CMC1, cm.CMC2, COUNT(bv.ID) AS num_of_article
-                        FROM chuyenmuc cm left join baiviet bv on cm.ID = bv.ID_ChuyenMuc
-                        GROUP BY cm.ID, cm.ChuyenMuc, cm.CMC1, cm.CMC2`);
+        return db.load(`SELECT * From ChuyenMuc`);
     },
 
     singel: id =>{
