7397c34c6266ef90906df015b1dab2d31e64e9d7
zSimpleMan <43574093+zSimpleMan@users.noreply.github.com>
2019-05-19 18:14:00 +0700

    cm

--- routes/chuyenmuc.route.js
+++ routes/chuyenmuc.route.js
@@ -6,16 +6,23 @@ var router = exress.Router();
 router.get('/:id/baiviet', (req, res) => {
     var id = req.params.id;
     var p = baivietModel.allByCat(id);
+    var q=chuyenmuc.singel(id);
+    var row;
+    q.then(rows1=>{
+        row=rows1;
+    });
 
 
-    p.then(rows => {
+    p.then(rows2 => {
+
         for (const cm of res.locals.lcChuyenmuc) {
             if (cm.ID == +id) {
                 cm.isActive = true;
             }
         }
         res.render('vwBaiviet/byCat', {
-            baiviet: rows,
+            baiviet: rows2,
+            CM:row[0]
         });
     }).catch(err => {
         console.log(err);
