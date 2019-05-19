ec2d9aefe254f4f6ddf91d2139156a135fef6039
hauphan15 <mrhauphan@gmail.com>
2019-05-19 18:17:45 +0700

    Update chuyenmuc.route.js

--- routes/chuyenmuc.route.js
+++ routes/chuyenmuc.route.js
@@ -2,11 +2,12 @@ var exress = require('express');
 var baivietModel = require('../models/baiviet.model');
 var router = exress.Router();
 
+
 router.get('/:id/baiviet', (req, res) => {
     var id = req.params.id;
     var p = baivietModel.allByCat(id);
-    p.then(rows => {
 
+    p.then(rows => {
         for (const cm of res.locals.lcChuyenmuc) {
             if (cm.ID == +id) {
                 cm.isActive = true;
