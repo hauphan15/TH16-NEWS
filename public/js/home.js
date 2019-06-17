e89eb9827c87fb3e683bfa3b2ed5a3c4143d1888
zSimpleMan <43574093+zSimpleMan@users.noreply.github.com>
2019-06-17 21:31:52 +0700

    hiệu ứng slick (trang chủ)

---
+++ public/js/home.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.xem-nhieu').slick({
+            nextArrow: '.next-xem-nhieu',
+            prevArrow: '.prev-xem-nhieu'
+    });
+    $('.moi-nhat').slick({
+        nextArrow: '.next-moi-nhat',
+        prevArrow: '.prev-moi-nhat'
+    });
+    $('.top-chuyen-muc').slick({
+        nextArrow:'.next-topcm',
+        prevArrow:'.prev-topcm'
+    })
+})
